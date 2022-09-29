535f3d31d066060c12b377532f56682d6c544d6c
suyh <su_787910081@sina.com>
2022-09-29 10:24:36 +0800

    Update CustomerEditorMain.java

--- demo-spring-debug/src/main/java/com/mashibing/selfEditor/CustomerEditorMain.java
+++ demo-spring-debug/src/main/java/com/mashibing/selfEditor/CustomerEditorMain.java
@@ -1,9 +1,18 @@
 package com.mashibing.selfEditor;
 
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.CustomEditorConfigurer;
+import org.springframework.beans.support.ResourceEditorRegistrar;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * 自定义属性编辑器，可以将配置文件中的字符串解析成一个vo 对象(Address)，并将其注入到bean 的属性中。
+ * <ol>
+ * <li>{@link AbstractApplicationContext#prepareBeanFactory(ConfigurableListableBeanFactory)} 预处理的时候添加了spring 实现的{@link ResourceEditorRegistrar}</li>
+ * <li>{@link CustomEditorConfigurer} 中配置用户实现的{@link ResourceEditorRegistrar} 以{@link BeanFactoryPostProcessor} 的身份</li>
+ * </ol>
  */
 public class CustomerEditorMain {
     public static void main(String[] args) {
