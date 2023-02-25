287e1c15bd0952b32f49f84f683101b6de8aa35f
苏云弘 <su_787910081@sina.com>
2023-02-25 22:33:16 +0800

    文件上传功能

--- spring-mvc-suyh/src/main/java/com/suyh/mvc/controller/DemoController.java
+++ spring-mvc-suyh/src/main/java/com/suyh/mvc/controller/DemoController.java
@@ -2,7 +2,9 @@ package com.suyh.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/suyh")
@@ -11,5 +13,14 @@ public class DemoController {
     @ResponseBody
     public String hello() {
         return "hello";
+    }
+
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public String fileUpload(@RequestPart(value = "file") MultipartFile upload) {
+        if (upload == null) {
+            return "failed";
+        }
+        return upload.getOriginalFilename();
     }
 }
